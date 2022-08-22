8d27d7a97f386b25d25b7e67aa8c4f98915fa56d
aqiux <40255611+aqiux@users.noreply.github.com>
2022-08-22 23:56:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
 def get_today():
-  todayss=str(datetime.date().today())
+  todayss=str(datetime.now())
   return todayss
 
 client = WeChatClient(app_id, app_secret)
